e5d426bf42dd4cff21575d553f94c5bfd73dbe33
Jalil-sayarh <asayarh@albertschool.com>
2024-12-12 13:00:56 +0100

    improved error handling

--- app.py
+++ app.py
@@ -2,29 +2,34 @@ from flask import Flask, request, jsonify
 from linkedin_api import Linkedin
 from linkedin_cookies import accounts_cookies
 from data_manipulation import extract_job_ids, fetch_job_details, filter_jobs_by_keywords
+
 app = Flask(__name__)
 
 # Route to authenticate and search jobs
 @app.route('/search_jobs', methods=['GET'])
 def search_jobs():
     # Get query parameters
-    username = request.args.get('username',"abdeljalil.sayarh@gmail.com")
-    password = request.args.get('password',"54321Nisk@")
+    username = request.args.get('username', "abdeljalil.sayarh@gmail.com")
+    password = request.args.get('password', "54321Nisk@")
     keywords = request.args.get('keywords')
     location = request.args.get('location')
     experience = request.args.getlist('experience')  # List of experience levels
     job_type = request.args.getlist('job_type')  # List of job types
     industries = request.args.getlist('industries')  # List of industry URN IDs
-    limit = int(request.args.get('limit', 10))  # Default limit to 10
-
-    if not username or not password:
-        return jsonify({"error": "Missing username or password"}), 400
+    limit = int(request.args.get('limit', 10))  # Default limit to 10 jobs
 
     try:
-        try:
-            selected_cookies = accounts_cookies.get(username)
-        except KeyError:
-            return jsonify({"error": "No cookies found for this account, please change the account or contact developper to manually add cookies"}), 400
+        # Check if the username exists in accounts_cookies
+        if username not in accounts_cookies:
+            available_accounts = list(accounts_cookies.keys())
+            return jsonify({
+                "error": f"No cookies found for this account: {username}.",
+                "available_accounts": available_accounts
+            }), 400
+
+        # Retrieve cookies for the specified username
+        selected_cookies = accounts_cookies[username]
+
         # Authenticate with LinkedIn
         linkedin = Linkedin(username, password, cookies=selected_cookies)
 
@@ -37,11 +42,14 @@ def search_jobs():
             industries=industries,
             limit=limit
         )
+
+        # Process job results
         job_ids = extract_job_ids(jobs)
-        jobs_details=fetch_job_details(linkedin, job_ids, ["title", "name", "url", "formattedLocation", "company_apply_url","text"])
+        jobs_details = fetch_job_details(linkedin, job_ids, ["title", "name", "url", "formattedLocation", "company_apply_url", "text"])
         relevant_jobs = filter_jobs_by_keywords(jobs_details)
-        return jsonify({
-            "relevant_jobs":relevant_jobs})
+
+        return jsonify({"relevant_jobs": relevant_jobs})
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
