e4e822bd665568795ab574140aa081baac57d77b
Jalil-sayarh <asayarh@albertschool.com>
2024-12-16 15:18:46 +0100

    finetuned new endpoints

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from data_manipulation import extract_job_ids, fetch_job_details, filter_jobs_by
 
 app = Flask(__name__)
 
-def get_authenticated_linkedin(username):
+def get_authenticated_linkedin(username, password):
     """Authenticate to LinkedIn using the provided username."""
     if username not in accounts_cookies:
         available_accounts = list(accounts_cookies.keys())
@@ -15,13 +15,14 @@ def get_authenticated_linkedin(username):
         }), 400
 
     selected_cookies = accounts_cookies[username]
-    linkedin = Linkedin(username, cookies=selected_cookies)
+    linkedin = Linkedin(username, password, cookies=selected_cookies)
     return linkedin, None
 
 # Endpoint to get job IDs based on search parameters
 @app.route('/get_job_ids', methods=['GET'])
 def get_job_ids():
     username = request.args.get('username', "abdeljalil.sayarh@gmail.com")
+    password = request.args.get('password', "54321Nisk@")
     keywords = request.args.get('keywords')
     location = request.args.get('location')
     experience = request.args.getlist('experience')
@@ -29,7 +30,7 @@ def get_job_ids():
     industries = request.args.getlist('industries')
     limit = int(request.args.get('limit', 10))
 
-    linkedin, error_response = get_authenticated_linkedin(username)
+    linkedin, error_response = get_authenticated_linkedin(username,password)
     if error_response:
         return error_response
 
@@ -55,13 +56,14 @@ def get_job_ids():
 def fetch_job_details_endpoint():
     data = request.json
     username = data.get('username', "abdeljalil.sayarh@gmail.com")
+    password = data.get('password', "54321Nisk@")
     job_ids = data.get('job_ids')
     keys_to_extract = ["title", "name", "url", "formattedLocation", "company_apply_url", "text"]
 
     if not job_ids or not isinstance(job_ids, list):
         return jsonify({"error": "Invalid or missing 'job_ids'. It should be a list of job IDs."}), 400
 
-    linkedin, error_response = get_authenticated_linkedin(username)
+    linkedin, error_response = get_authenticated_linkedin(username,password)
     if error_response:
         return error_response
 
